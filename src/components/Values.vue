4d6d135c35c4ad9ac0048ba19b02429840fb3455
Eligio Biasi <ex_98@live.it>
2023-06-26 18:09:42 +0200

    adding drop collapse menus

--- src/components/Values.vue
+++ src/components/Values.vue
@@ -12,26 +12,47 @@
                         Avada University is devoted to excellence in teaching learning, and research, and developing leaders in mnay disciplines who make a difference globally.
                     </p>
 
-                    <div>
+                    <div class="mb-1">
                         <i class="fa-solid fa-globe fa-lg pe-3" style="color: #f09a3e;"></i>
                         <span class="fw-bold">Internal focus</span>
                         <span class="grey-text"> - our students come from across the globe</span>
                     </div>
 
-                    <div>
-                        <i class="fa-solid fa-graduation-cap fa-lg pe-3" style="color: #f09a3e;"></i>
+                    <div class="mb-1">
+                        <i class="fa-solid fa-graduation-cap pe-3" style="color: #f09a3e;"></i>
                         <span class="fw-bold">Internal focus</span>
                         <span class="grey-text"> - our students come from across the globe</span>
                     </div>
-                    <div>
+
+                    <div class="mb-1">
                         <i class="fa-solid fa-address-book fa-lg pe-3" style="color: #f09a3e;"></i>
                         <span class="fw-bold">Internal focus</span>
                         <span class="grey-text"> - our students come from across the globe</span>
                     </div>
+
+                    <p>
+                        if you prefer to study in your own time and at your own speed then one of our e-learning packages may be right up your street.
+                    </p>
                 </div>
 
                 <div class="col-6">
-
+                    <h3 class="text-uppercase">
+                        our Values
+                    </h3>
+                    <div>
+                            <div class="p-2 d-flex align-items-center">
+                            <i class="fa-solid fa-plus fa-lg" style="color: #f09a3e;" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample"></i>
+                            <span class="text-uppercase fw-bold ms-3 fs-4">
+                                our mission
+                            </span>
+                    </div>
+                    <div class="collapse" id="collapseExample">
+                                <span class="card card-body">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus pariatur dignissimos aspernatur quasi officia recusandae quae consectetur minus, eum saepe quisquam ipsum, asperiores quia consequatur necessitatibus eaque nam praesentium quas!
+                                </span>
+                        </div>
+                    </div>
+                   
                 </div>
             </div>
         </div>
@@ -61,6 +82,11 @@ export default {
 
     h3{
         color: #333c4e;
+        margin: 0;
+    }
+
+    p{
+        margin-top: 2rem;
     }
 
     p,
