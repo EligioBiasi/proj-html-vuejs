2cbc7fbd33842c8f77a65eb2c671b19c6a9b423b
Eligio Biasi <ex_98@live.it>
2023-06-26 12:57:53 +0200

    added first element of topheader

--- src/components/TopHeader.vue
+++ src/components/TopHeader.vue
@@ -1,6 +1,19 @@
 <template>
     <div class="orange-background">
-        
+        <div class="container py-3">
+            <div class="text-white d-flex align-items-center">
+                <i class="fa-solid fa-phone fa-2xs" style="color: #ffffff;"></i>
+                <span class="d-flex align-items-center">1.800.567.8910
+                    <span class="gray-pill">24 hrs</span>
+                </span>
+                <i class="fa-solid fa-envelope fa-xs" style="color: #ffffff;"></i>
+                <span>Contact us</span>
+                <i class="fa-solid fa-graduation-cap fa-xs" style="color: #ffffff;"></i>
+                <span>Alumni</span>
+                <i class="fa-solid fa-landmark fa-xs" style="color: #ffffff;"></i>
+                <span>Campus</span>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -23,5 +36,12 @@ export default {
 <style lang="scss" scoped>
     div.orange-background{
         background-color: #f09a3e;
+    }
+
+    span.gray-pill{
+        padding: 0 .2rem;
+        background-color: lightgray;
+        font-size: .5rem;
+        border-radius: 12%;
     }
 </style>
